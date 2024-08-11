b4a6b6b0f663a86dfee5617157063b16aeaa011c
tiraaj18 <65433281+tiraaj18@users.noreply.github.com>
2024-08-11 11:46:59 +0000

    Used props and destructuring

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,107 +1,54 @@
-import { useState } from "react";
 import femaleProfile from "../dist/assets/Images/femaleProfile.jpg";
 import maleProfile from "../dist/assets/Images/maleProfile.jpg";
 
-const Employees = () => {
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      fullName: "Bob Jones",
-      designation: "JavaScript Developer",
-      gender: "male",
-      teamName: "TeamA",
-    },
-    {
-      id: 2,
-      fullName: "Jill Bailey",
-      designation: "Node Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 3,
-      fullName: "Gail Shepherd",
-      designation: "Java Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 4,
-      fullName: "Sam Reynolds",
-      designation: "React Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 5,
-      fullName: "David Henry",
-      designation: "DotNet Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 6,
-      fullName: "Sarah Blake",
-      designation: "SQL Server DBA",
-      gender: "female",
-      teamName: "TeamB",
-    },
-    {
-      id: 7,
-      fullName: "James Bennet",
-      designation: "Angular Developer",
-      gender: "male",
-      teamName: "TeamC",
-    },
-    {
-      id: 8,
-      fullName: "Jessica Faye",
-      designation: "API Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 9,
-      fullName: "Lita Stone",
-      designation: "C++ Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 10,
-      fullName: "Daniel Young",
-      designation: "Python Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 11,
-      fullName: "Adrian Jacobs",
-      designation: "Vue Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 12,
-      fullName: "Devin Monroe",
-      designation: "Graphic Designer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-  ]);
-
+  const Employees = ({ employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange }) => {
+  
   return (
-    <main>
-      {employees.map((employee) => (
-        <>
-          <p>{employee.fullName}</p>
-          {employee.gender === "male" ? (
-            <img src={maleProfile} />
-          ) : (
-            <img src={femaleProfile} />
-          )}
-        </>
-      ))}
+    <main className="container">
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-6">
+          <select
+            className="form-select form-select-lg"
+            value={selectedTeam}
+            onChange={handleTeamSelectionChange}
+          >
+            <option value="TeamA">Team A</option>
+            <option value="TeamB">Team B</option>
+            <option value="TeamC">Team C</option>
+            <option value="TeamD">Team D</option>
+          </select>
+        </div>
+      </div>
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+          <div className="card-collection">
+            {employees.map((employee) => (
+              <div
+                id={employee.id}
+                className={
+                  employee.teamName === selectedTeam
+                    ? "card m-2 standout"
+                    : "card m-2"
+                }
+                style={{ cursor: "pointer" }}
+                onClick={handleEmployeeCardClick}
+              >
+                {employee.gender === "male" ? (
+                  <img src={maleProfile} className="card-img-top" />
+                ) : (
+                  <img src={femaleProfile} className="card-img-top" />
+                )}
+                <div className="card-body">
+                  <h5 className="card-title">Name: {employee.fullName}</h5>
+                  <p className="card-text">
+                    <b>Designation: </b>{employee.designation}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Header = () => {
+const Header = ({selectedTeam, teamMemberCount}) => {
   return (
-    <header>
-      <h1 className="mt-4 p5 bg-dark text-white rounded">Headeer</h1> 
+    <header className='container'>
+      <div className='row justify-content-center mt-3 mb-4'>
+        <div className='col-8'>
+          <h1>Team Member Allocation</h1> 
+          <h3>{selectedTeam} has {teamMemberCount}</h3> 
+        </div>
+      </div>  
     </header>
   );
 };
